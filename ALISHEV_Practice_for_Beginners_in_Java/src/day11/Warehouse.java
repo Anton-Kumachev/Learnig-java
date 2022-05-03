ec48dfd4975d74bc71dc498fe1eb522aff82486d
Anton-Kumachev <anton.kumachev@inbox.ru>
2022-05-03 19:47:08 +0700

    Added day11

---
+++ ALISHEV_Practice_for_Beginners_in_Java/src/day11/Warehouse.java
@@ -0,0 +1,30 @@
+package day11;
+
+public class Warehouse {
+    private int balance; //доход от доставленных заказов
+    private int countOrder; //количество собранных заказов
+
+    @Override
+    public String toString() {
+        return "Warehouse{" +
+                "balance=" + balance +
+                ", countOrder=" + countOrder +
+                '}';
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public int getCountOrder() {
+        return countOrder;
+    }
+
+    public void setCountOrder(int countOrder) {
+        this.countOrder = countOrder;
+    }
+}
